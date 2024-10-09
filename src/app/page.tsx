430e2254f52e4c501fd5eae46ce77ec9b1d2f803
RintaroNasu <rinta022823@gmail.com>
2024-10-09 18:07:02 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center gap-2">
+      <div>
+        <button>ログインページへ</button>
+      </div>
+      <div>
+        <button>新規登録ページへ</button>
+      </div>
+    </div>
+  );
+}
