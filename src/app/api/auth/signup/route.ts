4c8d764442a76997231a7c2009495808572fe7d7
RintaroNasu <rinta022823@gmail.com>
2024-10-11 21:05:28 +0900

    jwtを実装

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../../prisma/prisma";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export async function POST(req: Request) {
   const { email, password } = await req.json();
@@ -10,6 +13,10 @@ export async function POST(req: Request) {
         password,
       },
     });
+    if (JWT_SECRET) {
+      const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: "1h" });
+      return NextResponse.json({ message: "サインアップ成功", user, token }, { status: 200 });
+    }
     return NextResponse.json(user);
   } catch (e) {
     return NextResponse.json(e);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,47 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    setIsLoggedIn(!!token);
+  }, []);
+
+  const onClickLogout = () => {
+    localStorage.removeItem("access_token");
+    console.log("ログアウトしました");
+    setIsLoggedIn(false);
+    router.push("/");
+  };
+
   return (
-    <div className="flex justify-center gap-2 mt-10">
-      <div>
-        <Link href="/user/sign_in" className="rounded-[4px] bg-[rgba(0,164,150,1)] py-2 font-semibold text-white hover:bg-[rgba(0,106,118,1)] disabled:bg-[rgba(0,164,150,1) ml-2 h-11 w-[100px] px-4">
-          ログイン
-        </Link>
-      </div>
-      <div>
-        <Link href="/user/sign_up" className="rounded-[4px] border border-fg-primary bg-white py-2 font-semibold text-fg-primary hover:bg-gray-300 disabled:opacity-[0.38] ml-2 h-11 w-[100px] px-4">
-          新規登録
-        </Link>
-      </div>
+    <div className="flex justify-center items-center gap-2 mt-10">
+      {!isLoggedIn && (
+        <>
+          <div>
+            <Link href="/user/sign_in" className="rounded-[4px] bg-[rgba(0,164,150,1)] py-2 font-semibold text-white hover:bg-[rgba(0,106,118,1)] disabled:bg-[rgba(0,164,150,1) ml-2 h-11 w-[100px] px-4">
+              ログイン
+            </Link>
+          </div>
+          <div>
+            <Link href="/user/sign_up" className="rounded-[4px] border border-fg-primary bg-white py-2 font-semibold text-fg-primary hover:bg-gray-300 disabled:opacity-[0.38] ml-2 h-11 w-[100px] px-4">
+              新規登録
+            </Link>
+          </div>
+        </>
+      )}
+      {isLoggedIn && (
+        <div>
+          <button onClick={onClickLogout} className="rounded-[4px] bg-[rgba(0,164,150,1)] py-2 font-semibold text-white hover:bg-[rgba(0,106,118,1)] disabled:bg-[rgba(0,164,150,1) ml-2 h-11 w-[120px] px-4">
+            ログアウト
+          </button>
+        </div>
+      )}
     </div>
   );
 }
